c9f8decf4977a893f6957a4e622229301b21793d
InkyGod03 <95667342+InkyGod03@users.noreply.github.com>
2022-04-15 14:13:54 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
+const {
+	default: makeWASocket,
+	useSingleFileAuthState,
+	DisconnectReason,
+} = require('@adiwajshing/baileys')
+const fs = require('fs')
+const P = require('pino')
+const qrcode = require('qrcode-terminal')
 
+const { state, saveState } = useSingleFileAuthState('./session.json')
+
+const start = () => {
+  const inky = makeWASocket({
+		logger: P({ level: 'silent' }),
+		printQRInTerminal: true,
+		auth: state,
+	})
+  
+  inky.ev.on('connection.update', (update) => {
+		const { connection, lastDisconnect } = update
+		if (connection === 'close') {
+			lastDisconnect.error?.output?.statusCode !== DisconnectReason.loggedOut ? start() : console.log('El bot ha sido desconectado')
+		} else if (connection === 'open') {
+			console.log('Bot conectado')
+		}
+	})
+	
+	inky.ev.on('creds.update', saveState)
+}
