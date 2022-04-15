64cdb16a6cdc87385c2883ef4031ea77da59fc4b
InkyGod03 <95667342+InkyGod03@users.noreply.github.com>
2022-04-15 14:26:53 -0400

    Update index.js

--- index.js
+++ index.js
@@ -9,6 +9,9 @@ const qrcode = require('qrcode-terminal')
 
 const { state, saveState } = useSingleFileAuthState('./session.json')
 
+const prefix = '.'
+const ownerNumber = ['50760303041']
+
 const start = () => {
 	const conn = makeWASocket({
 		logger: P({ level: 'silent' }),
@@ -26,6 +29,53 @@ const start = () => {
 	})
 	
 	conn.ev.on('creds.update', saveState)
+	
+	conn.ev.on('messages.upsert', async(mek) => {
+		try {
+			mek = mek.messages[0]
+			if (!mek.message) return
+			
+			mek.message = (Object.keys(mek.message)[0] === 'ephemeralMessage') ? mek.message.ephemeralMessage.message : mek.message
+			if (mek.key && mek.key.remoteJid === 'status@broadcast') return
+			const type = Object.keys(mek.message)[0]
+			const content = JSON.stringify(mek.message)
+			const from = mek.key.remoteJid
+			
+			const quoted = type == 'extendedTextMessage' && mek.message.extendedTextMessage.contextInfo != null ? mek.message.extendedTextMessage.contextInfo.quotedMessage || [] : []
+			const body = (type === 'conversation') ? mek.message.conversation : (type === 'extendedTextMessage') ? mek.message.extendedTextMessage.text : (type == 'imageMessage') && mek.message.imageMessage.caption ? mek.message.imageMessage.caption : (type == 'videoMessage') && mek.message.videoMessage.caption ? mek.message.videoMessage.caption : ''
+			
+			const isCmd = body.startsWith(prefix)
+			const command = isCmd ? body.slice(prefix.length).trim().split(' ').shift().toLowerCase() : ''
+			
+			const args = body.trim().split(/ +/).slice(1)
+			const q = args.join(' ')
+			const isGroup = from.endsWith('@g.us')
+			const sender = mek.key.fromMe ? (conn.user.id.split(':')[0]+'@s.whatsapp.net' || conn.user.id) : (mek.key.participant || mek.key.remoteJid)
+			const senderNumber = sender.split('@')[0]
+			const botNumber = conn.user.id.split(':')[0]
+			const pushname = mek.pushName || 'Sin Nombre'
+			
+			const isMe = botNumber.includes(senderNumber)
+			const isOwner = ownerNumber.includes(senderNumber) || isMe
+			
+			const reply = async(teks) => {
+				await conn.sendMessage(from, { text: teks }, { quoted: mek })
+			}
+			
+			switch (command) {
+
+case 'hola':
+reply('Hola como estas? :D')
+break
+
+			}
+			
+		} catch (e) {
+			const isError = String(e)
+			
+			console.log(isError)
+		}
+	}
 }
 
 start()
